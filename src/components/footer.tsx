fe64bb78027e08431f3079a530296f6952c729c8
yudhisrana <dipoyudhisrana@gmail.com>
2025-08-21 15:17:44 +0700

    fix title name & title name footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="bg-gray-800 py-8 text-gray-300">
       <div className="mx-auto flex max-w-7xl items-center justify-center px-4">
         <div className="">
-          © {new Date().getFullYear()} CrayonTag. All rights reserved.
+          © {new Date().getFullYear()} Crayontag. All rights reserved.
         </div>
       </div>
     </footer>
